3eae70379e01729c18ea99b51517b6acfea323e7
Serega Balyuk <seregabalyuk@gmail.com>
2025-04-10 14:17:53 +0800

    redatc gogooooogog

--- scripts/createData.py
+++ scripts/createData.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
-data = pd.read_csv("manga.csv")
+data = pd.read_csv("../data/manga.csv")
 
 data2 = data[["title", "type","status" , "genres", "themes",
               "score", "scored_by", "volumes", "chapters",
               "start_date", "end_date"]]
-data3 = data2.dropna()
-print(data3.count())
+              
+# data2[['end_date']].fillna("2024-04-02 18:07:42+00:00")
+# data2['start_date'].fillna("2024-04-02 18:07:42+00:00")
+data3 = data2.dropna(subset=["title", "type","status" , "genres", "themes",
+              "score", "scored_by", "volumes", "chapters"])
+data4 = data3.fillna("2023-04-02")
+print(data4.count())
 
-out = data3#.sample(10000)
+out = data4.sample(1000)
 out.to_csv("../data/final.csv")
 
 
