20adc7a4849d0a096502241edcade8db11bb6c41
Serega Balyuk <seregabalyuk@gmail.com>
2025-05-29 00:31:51 +0800

    upd lab5

--- scripts/createData.py
+++ scripts/createData.py
@@ -8,9 +8,10 @@ data2 = data[["title", "type","status" , "genres", "themes",
               
 # data2[['end_date']].fillna("2024-04-02 18:07:42+00:00")
 # data2['start_date'].fillna("2024-04-02 18:07:42+00:00")
-data3 = data2.dropna(subset=["title", "type","status" , "genres", "themes",
+data3 = data2.dropna(subset=["title", "type" , "genres", "themes",
               "score", "scored_by", "volumes", "chapters"])
 data4 = data3.fillna("2023-04-02")
+print(data['status'].unique())
 print(data4.count())
 
 out = data4.sample(1000)
