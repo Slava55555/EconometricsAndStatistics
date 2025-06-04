0afad87c694a6f6a6bab77979f7141da94ce5993
Serega Balyuk <seregabalyuk@gmail.com>
2025-06-04 21:21:50 +0800

    add lab_e1

---
+++ scripts/createData.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from collections import Counter
+
+data = pd.read_csv("data/manga.csv")
+
+data2 = data[["title", "type","status" , "genres", "themes",
+              "score", "scored_by", "volumes", "chapters",
+              "start_date", "end_date"]]
+              
+# data2[['end_date']].fillna("2024-04-02 18:07:42+00:00")
+# data2['start_date'].fillna("2024-04-02 18:07:42+00:00")
+data3 = data2.dropna(subset=["title", "type" , "genres", "themes",
+              "score", "scored_by", "volumes", "chapters"])
+data4 = data3.fillna("2023-04-02")
+print(data['status'].unique())
+print(data4.count())
+data["start_date"] = pd.to_datetime(data["start_date"])
+data["end_date"] = pd.to_datetime(data["end_date"])
+data["genres"] = data["genres"].apply(eval)
+data["themes"] = data["themes"].apply(eval)
+
+all_themes = [theme for sublist in data["themes"] for theme in sublist]
+all_genres = [genre for sublist in data["genres"] for genre in sublist]
+
+genre_counts = Counter(all_genres)
+theme_counts = Counter(all_themes)
+
+genre_df = pd.DataFrame(genre_counts.items(), columns=["Genre", "Count"])
+genre_df = genre_df.sort_values(by="Count", ascending=False)
+
+theme_df = pd.DataFrame(theme_counts.items(), columns=["Theme", "Count"])
+theme_df = theme_df.sort_values(by="Count", ascending=False)
+
+view= 5
+top_themes = theme_df.head(view)
+top_genres = genre_df.head(view)
+
+print(top_genres['Genre'].unique())
+print(top_themes['Theme'].unique())
+
+
+for genre in  top_genres['Genre']:
+  print(genre)
+  data4[f'has {genre}'] = data4['genres'].apply(lambda x: 1 if genre in x else 0)
+for theme in  top_themes['Theme']:
+  data4[f'has {theme}'] = data4['themes'].apply(lambda x: 1 if theme in x else 0)
+
+
+
+sampled_data = data4.groupby('type', group_keys=False).apply(lambda x: x.sample(min(30, len(x))))
+print(sampled_data['type'].value_counts())
+
+print(sampled_data['status'].unique())
+print(sampled_data)
+
+sampled_data.to_csv("data/final.csv")
+
+
